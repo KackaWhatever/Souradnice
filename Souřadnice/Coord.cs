aedb2e7532d373e50124b71bc783165225dfb1e8
katerina.huskova.022 <katerina.huskova.022@PC2023-10>
2024-01-18 09:18:39 +0100

    Add project files.

---
+++ Souřadnice/Coord.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Souřadnice;
+
+namespace Souřadnice
+{
+    public class Coord : IEquatable<Coord>, IComparable<Coord>
+    {
+        public Coord(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public bool Equals(Coord other)
+        {
+            if (other == null) 
+                return false;
+            return true;
+        }
+        /*
+        public static bool operator >(Coord operator1, Coord operator2)
+        {
+            return operator1.CompareTo(operator2) > 0;
+        }
+
+        public int CompareTo(Coord other)
+        {
+            if (other == null) return 1;
+            return X.CompareTo(other.X);
+        }*/
+    }
+}
---
+++ Souřadnice/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Souřadnice;
+
+Coord c1 = new Coord(10, 10);
+Coord c2 = new Coord(10, 10);
+
+Console.WriteLine(c1 == c2); // False
+Console.WriteLine(c2.Equals(c2));   //True
